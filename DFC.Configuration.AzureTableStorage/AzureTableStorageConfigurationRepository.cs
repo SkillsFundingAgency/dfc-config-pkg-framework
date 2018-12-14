97ae617922e6c809dc4578c09882621112e923f5
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-12-14 09:09:37 +0000

    initial check in

---
+++ DFC.Configuration.AzureTableStorage/AzureTableStorageConfigurationRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DFC.Configuration.AzureTableStorage
+{
+    public class AzureTableStorageConfigurationRepository : IConfigurationRepository
+    {
+        private CloudStorageAccount _storageAccount;
+
+        public AzureTableStorageConfigurationRepository()
+            : this(CloudConfigurationManager.GetSetting("ConfigurationStorageConnectionString"))
+        {
+        }
+        public AzureTableStorageConfigurationRepository(string storageConnectionString)
+        {
+            _storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+        }
+
+        public string Get(string serviceName, string environmentName, string version)
+        {
+            var table = GetTable();
+            var operation = GetOperation(serviceName, environmentName, version);
+            var result = table.Execute(operation);
+
+            var configItem = (ConfigurationItem)result.Result;
+            return configItem?.Data;
+        }
+        public async Task<string> GetAsync(string serviceName, string environmentName, string version)
+        {
+            var table = GetTable();
+            var operation = GetOperation(serviceName, environmentName, version);
+            var result = await table.ExecuteAsync(operation);
+
+            var configItem = (ConfigurationItem)result.Result;
+            return configItem?.Data;
+        }
+
+
+        private CloudTable GetTable()
+        {
+            var tableClient = _storageAccount.CreateCloudTableClient();
+            return tableClient.GetTableReference("Configuration");
+        }
+        private TableOperation GetOperation(string serviceName, string environmentName, string version)
+        {
+            return TableOperation.Retrieve<ConfigurationItem>(environmentName, $"{serviceName}_{version}" );
+        }
+    }
+}
---
+++ DFC.Configuration.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFA.NCS.Configuration.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFA.NCS.Configuration.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e12f5443-ee39-45a6-9261-abda2804c6f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
